a690fdc03c669e9393210054eb806785a281bd34
jsvirtane <virtanenjami@gmail.com>
2021-11-18 14:49:41 +0200

    Misspelled parameter as variable, changed those

--- RisingStar.py
+++ RisingStar.py
@@ -1,18 +1,14 @@
 import requests
 from datetime import datetime, timezone, date
 
-# Variable: User input dates
+# Parameter: User input dates
 # Return: [] where [0] = start date, [1] end date
-
-
 def getDates():
     startDate = input('Enter start date in DD.MM.YYYY format: ')
     endDate = input('Enter end date in DD.MM.YYYY format: ')
     return [startDate, endDate]
 
 # Return: [] where [0] = start date as Unix timestamp, [1] = end date as Unix timestamp
-
-
 def getDatesAsUnix():
     startDate, endDate = getDates()
     startDay, startMonth, startYear = map(int, startDate.split('.'))
@@ -24,35 +20,27 @@ def getDatesAsUnix():
     return [startTimeStamp, endTimeStamp]
 
 # Return: raw data as a json. Includes prices, market cap, total volumes
-
-
 def getRawData():
     startDate, endDate = getDatesAsUnix()
     requestUrl = f'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from={startDate}&to={endDate}'
     request = requests.get(requestUrl)
     return request.json()
 
-# Variable: timestamp = Unix timestamp as seconds,
+# Parameter: timestamp = Unix timestamp as seconds,
 # Return: Date in DD.MM.YYYY-format
-
-
 def UnixToDate(timestamp):
     date = datetime.utcfromtimestamp(timestamp)
     formattedDate = date.strftime("%d.%m.%Y")
     return formattedDate
 
-# Variable: Unix timestamp as milliseconds
+# Parameter: Unix timestamp as milliseconds
 # Return: Unix timestamp as seconds
-
-
 def timestampConverter(milliseconds):
     seconds = milliseconds/1000.0
     return seconds
 
-# Variable: List = [DD.MM.YYYY, DD.MM.YYYY], where [0] = startDay, [1] = endDay
+# Parameter: List = [DD.MM.YYYY, DD.MM.YYYY], where [0] = startDay, [1] = endDay
 # Return: Number of days between given dates
-
-
 def daysBetween(list):
     startDate, endDate = list
     startDay, startMonth, startYear = map(int, startDate.split('.'))
@@ -61,10 +49,8 @@ def daysBetween(list):
         date(startYear, startMonth, startDay)
     return(delta.days)
 
-# Variable: Json = Raw data from 'getRawData'-function
+# Parameter: Json = Raw data from 'getRawData'-function
 # Return: [] that have []'s as a values where [0] = day(UNIX), [1] = price of bitcoin
-
-
 def priceData(json):
     prices = json['prices']
     days = daysBetween([UnixToDate(timestampConverter(
